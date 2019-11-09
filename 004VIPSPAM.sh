60e5c3e3a57e1897b7dc02def6b6199578bf7598
Mr.004 <57171003+Mr-004Tols@users.noreply.github.com>
2019-11-09 10:21:27 +0700

    Update 004VIPSPAM.sh

--- 004VIPSPAM.sh
+++ 004VIPSPAM.sh
@@ -1,3 +1,96 @@
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo
+echo
+python2 meizu.py
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v("   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
 #!/system/xbin/bash
 clear
 python2 meizu.py
@@ -7,7 +100,7 @@ chmod +x ./install
 apt install ./smsid_1.1_all.deb
 clear
 
-toilet -f slant --gay "VIOSpam"
+toilet -f slant --gay "VIPSpam"
 sleep 1
 echo "\033[33;1m Tols Untuk spam"
 sleep 1
